edde84cc36a34f8793009c22e99f51de05d07f74
Evan Yang <evanjyang4@gmail.com>
2025-02-28 14:15:27 -0800

    root route works

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,8 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
   ],
 });
