32677c3899a73fab166843becab4ae367916d87f
Evan Yang <evanjyang4@gmail.com>
2025-03-20 00:09:05 -0700

    env file

--- config/database.js
+++ config/database.js
@@ -1,11 +1,16 @@
 // config/database.js
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("Grocery", "root", "root", {
-  host: "127.0.0.1",
-  port: 3307,
-  dialect: "mysql",
-  logging: console.log, // Set to false to disable logging
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "127.0.0.1",
+    port: 3307,
+    dialect: "mysql",
+    logging: console.log, // Set to false to disable logging
+  }
+);
 
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3307;
 const db = require('./models');
 const routes = require('./routes'); // Import the main routes file
 
