2314c0afde6eee37f52572410f5f7d152eb6f749
Evan Yang <evanjyang4@gmail.com>
2025-03-22 13:42:15 -0700

    set up - login page - fix homepage

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -32,19 +32,6 @@ const Layout = () => (
                 About
               </Link>
             </li>
-            <li>
-              <Link
-                to="/search"
-                search={{ q: "" }}
-                className="hover:bg-gray-100 hover:text-gray-900 text-gray-700 border border-transparent hover:border-gray-300 transition-colors duration-200 px-7 py-4 rounded-md"
-                activeProps={{
-                  className:
-                    "font-bold text-gray-900 border border-gray-300 bg-gray-100",
-                }}
-              >
-                Search
-              </Link>
-            </li>
           </ul>
         </nav>
       </div>
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -7,16 +7,16 @@ const Home = () => {
       <h2 className="text-4xl font-extrabold text-gray-900 mb-6">
         Welcome to Your Online Grocery Store
       </h2>
-      <p className="text-lg text-gray-700 mb-8">
+      <p className="text-lg text-gray-700 mb-15">
         Get fresh produce, pantry staples, and more delivered right to your
         door.
       </p>
       <Link
         to="/search"
         search={{ q: "" }}
-        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 m-10"
+        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg px-10 py-5 text-center me-2 mb-2 m-10"
       >
-        Start Shopping Now
+        Log In
       </Link>
     </section>
   );
