f936888b87d8c0d1f2762346134790c9f16da316
Evan Yang <evanjyang4@gmail.com>
2025-03-22 15:21:15 -0700

    login page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -12,9 +12,9 @@ const Home = () => {
         door.
       </p>
       <Link
-        to="/search"
+        to="/login"
         search={{ q: "" }}
-        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg px-10 py-5 text-center me-2 mb-2 m-10"
+        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-10 py-5 text-center me-2 mb-2 m-10"
       >
         Log In
       </Link>
---
+++ client/src/routes/login.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import { createFileRoute, Link } from "@tanstack/react-router";
+
+const RouteComponent = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="max-w-sm mx-auto">
+      <form>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder=""
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700"
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ms-2 mb-2 text-sm font-medium text-gray-900"
+          >
+            Remember me
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-7 py-3 text-center mb-5"
+        >
+          Login
+        </button>
+      </form>
+      <Link
+        to="/create-account"
+        className="font-medium text-blue-600 hover:underline"
+      >
+        Create an Account
+      </Link>
+    </div>
+  );
+};
+
+export const Route = createFileRoute("/login")({
+  component: RouteComponent,
+});
